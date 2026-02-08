c01dc81690e34017074891441d1eccb2931e5a77
Brian Taylor <katmandoo212@gmail.com>
2026-02-07 21:53:13 -0500

    feat: export RerankTechnique and CompressTechnique

--- techniques/__init__.py
+++ techniques/__init__.py
@@ -4,10 +4,14 @@ from techniques.naive_rag import NaiveRAGTechnique
 from techniques.hyde import HyDETechnique
 from techniques.multi_query import MultiQueryTechnique
 from techniques.hybrid import HybridTechnique
+from techniques.rerank import RerankTechnique
+from techniques.compress import CompressTechnique
 
 __all__ = [
     "NaiveRAGTechnique",
     "HyDETechnique",
     "MultiQueryTechnique",
     "HybridTechnique",
+    "RerankTechnique",
+    "CompressTechnique",
 ]
